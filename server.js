de88b3a15cf5f82d334715489d91227b0b6b0138
White Buffalo <apothocare@gmail.com>
2022-10-19 20:54:31 -0500

    Sync up

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ const PORT = process.env.PORT || 3001;
 const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
+
 const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
+    secret: process.env.Secret,
+    cookie: {
+      expires: 1200000
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
 };
 
 app.use(session(sess));
