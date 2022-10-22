fdb3da70bf99133142031013222e3e2cfcd162cd
Vincent Toon <vincenttoon22@gmail.com>
2022-10-22 15:00:20 -0500

    first dropdown added

---
+++ public/javascript/dropdown.js
@@ -0,0 +1,11 @@
+document.querySelector('.select-field').addEventListener('click',()=>{
+    document.querySelector('.list').classList.toggle('show');
+    document.querySelector('.down-arrow').classList.toggle('rotate180');
+
+});
+
+document.querySelector('.select-field-2').addEventListener('click',()=>{
+    document.querySelector('.list').classList.toggle('show');
+    document.querySelector('.down-arrow').classList.toggle('rotate180');
+
+});
