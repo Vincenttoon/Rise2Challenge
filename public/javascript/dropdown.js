e2dbd5af7a959941ff1676cd98bafaf0eacfa703
Vincent Toon <vincenttoon22@gmail.com>
2022-10-23 23:19:35 -0500

    workout page functionality completed

--- public/javascript/dropdown.js
+++ public/javascript/dropdown.js
@@ -9,3 +9,24 @@ document.querySelector('.select-field-2').addEventListener('click',()=>{
     document.querySelector('.down-arrow-2').classList.toggle('rotate180');
 
 });
+
+
+document.querySelector('.create-exercise-btn').addEventListener('click', ()=>{
+    document.querySelector('.hide-exercise').classList.toggle('show');
+});
+
+document.querySelector('.create-workout-btn').addEventListener('click', ()=>{
+    document.querySelector('.hide-workout').classList.toggle('show');
+});
+
+document.querySelector('.create-workout-submit').addEventListener('click', ()=> {
+    document.querySelector('.hide-workout').classList.toggle('hide');
+
+    // Push new workout to Workout data workout.name
+});
+
+document.querySelector('.create-exercise-submit').addEventListener('click', ()=> {
+        // Push new workout to Workout data workout.name
+        
+    document.querySelector('.hide-exercise').classList.toggle('hide');
+});
