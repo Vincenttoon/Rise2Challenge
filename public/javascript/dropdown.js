869ba1d292fcecfec61dc8215df97887ced955c2
Vincent Toon <vincenttoon22@gmail.com>
2022-10-23 22:15:14 -0500

    workout page setup for now

--- public/javascript/dropdown.js
+++ public/javascript/dropdown.js
@@ -5,7 +5,7 @@ document.querySelector('.select-field').addEventListener('click',()=>{
 });
 
 document.querySelector('.select-field-2').addEventListener('click',()=>{
-    document.querySelector('.list').classList.toggle('show');
-    document.querySelector('.down-arrow').classList.toggle('rotate180');
+    document.querySelector('.list-2').classList.toggle('show');
+    document.querySelector('.down-arrow-2').classList.toggle('rotate180');
 
 });
