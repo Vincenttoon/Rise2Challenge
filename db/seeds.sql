de49cd78368694071a3c3ba94fec826d5db1c391
Vincent Toon <vincenttoon22@gmail.com>
2022-10-24 13:49:38 -0500

    more seeds for changing

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO exercises (ex_name, intensity, ex_type)
+VALUES
+  ('Deadlift', 'High', 'Full body');
+
+
+INSERT INTO user (id, username, email, password)
+VALUES
+  ('1', "Vincent", 'test@email.com', 'testpw')
