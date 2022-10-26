81c91113c7c8f4dd2789a4e5651b475316510dc6
Vincent Toon <vincenttoon22@gmail.com>
2022-10-26 16:30:24 -0500

    pushy push for Josh

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,11 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes");
-const userInfoRoutes = require("./user-info-routes");
 const workoutPageRoutes = require('./workout-page-routes');
 const yourPageRoutes = require('./your-page-routes')
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
-router.use("/user-info", userInfoRoutes);
 router.use('/build-workout', workoutPageRoutes );
 
 router.use((req, res) => {
